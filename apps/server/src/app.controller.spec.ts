8b145fb2e8ba4cb5b8f29685a94a1af347741f93
Anton Bakhurynskyi <antoncodes@outlook.com>
2022-02-01 17:36:28 +0200

    moving husky to dev dependencies

--- apps/server/src/app.controller.spec.ts
+++ apps/server/src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should be defined', () => {
       expect(appController).toBeDefined();
     });
   });
