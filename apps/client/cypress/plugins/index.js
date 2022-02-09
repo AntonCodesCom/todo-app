a2661cf0e29a1a32af7bc2f8396128862ca88b55
Anton Bakhurynskyi <toxa16@outlook.com>
2022-02-09 11:28:01 +0000

    Merged PR 52: update env configuration
    update env configuration

--- apps/client/cypress/plugins/index.js
+++ apps/client/cypress/plugins/index.js
@@ -12,7 +12,7 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-require('dotenv').config();
+require('dotenv-flow').config();
 
 /**
  * @type {Cypress.PluginConfig}
